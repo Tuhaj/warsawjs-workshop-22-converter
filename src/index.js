f1205a84a1ee8fce1a79b6c9a4c4e965ddd53441
Piotr Zientara <314zientara@gmail.com>
2018-07-21 14:54:37 +0200

    Refactor converter, add project setup

---
+++ src/index.js
@@ -0,0 +1,36 @@
+class WJSConverter {
+  constructor(convertObject, rateList) {
+    this.rateList = rateList;
+    this.convertObject = convertObject;
+  }
+
+  convert(value, currencyFrom, currencyTo) {
+    currencyFrom = currencyFrom.toLowerCase();
+    currencyTo = currencyTo.toLowerCase();
+
+    if(currencyFrom !== currencyTo) {
+      value *= this.convertObject[currencyTo][currencyFrom];
+    }
+    return value.toFixed(2);
+  }
+
+  generateConvertTable(currentRateContainer, inputCurrency) {
+    currentRateContainer.innerText = inputCurrency.value;
+    this.rateList.innerHTML = null;
+    Object.keys(this.convertObject[inputCurrency.value.toLowerCase()]).map(currencyName => {
+      const listElement = document.createElement('li');
+      const currencyNameContainer = document.createElement('span');
+      const currencyRate = document.createElement('span');
+
+      currencyNameContainer.innerText = currencyName;
+      currencyRate.innerText = this.convertObject[inputCurrency.value.toLowerCase()][currencyName];
+
+
+      listElement.appendChild(currencyNameContainer);
+      listElement.appendChild(currencyRate);
+
+      this.rateList.appendChild(listElement);
+    });
+  }
+}
+
