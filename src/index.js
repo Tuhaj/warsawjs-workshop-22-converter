7da0a261323b7469901fcf10ad92ca56c89750b7
Piotr Zientara <314zientara@gmail.com>
2018-07-21 16:33:16 +0200

    Refactor: add method createConvertObject

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 class WJSConverter {
-  constructor(convertObject, rateList) {
+  constructor(rateList) {
     this.rateList = rateList;
-    this.convertObject = convertObject;
+    this.convertObject = this.fetchConvertObject();
   }
 
   convert(value, currencyFrom, currencyTo) {
@@ -31,6 +31,17 @@ class WJSConverter {
 
       this.rateList.appendChild(listElement);
     });
+  }
+
+  fetchConvertObject() {
+    return {
+      /*currencyTo: { currencyFrom: rate }*/
+      'pln': { 'eur': 4.25, 'usd': 3.59, 'gbp': 4.62, 'chf': 3.69 },
+      'eur': { 'pln': 0.23, 'usd': 0.84, 'gbp': 1.08, 'chf': 0.86 },
+      'usd': { 'eur': 1.18, 'pln': 0.27, 'gbp': 1.28, 'chf': 1.03 },
+      'gbp': { 'pln': 0.21, 'eur': 0.92, 'usd': 0.77, 'chf': 0.80 },
+      'chf': { 'pln': 0.27, 'eur': 1.16, 'usd': 0.97, 'gbp': 1.25 }
+    };
   }
 }
 
